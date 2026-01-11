07d386dd1eb8ae99fb786a7f8a3920b9ac84a222
VOCACIONALGH <Cc20230806vocacional@hotmail.com>
2026-01-11 11:23:53 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('menu-app-cache').then(cache => {
+      return cache.addAll([
+        'index.html',
+        'layout.css',
+        'main.js',
+        'import.js',
+        'export.js'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
