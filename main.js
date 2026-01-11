7a21981ee74e10c7a62c2c2e97e2276b3fb56cdb
VOCACIONALGH <Cc20230806vocacional@hotmail.com>
2026-01-11 11:43:04 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,18 +2,10 @@ if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('service-worker.js');
 }
 
-const menu = document.getElementById('app');
-const telaRegistro = document.getElementById('telaRegistro');
-
 const btnRegistro = document.getElementById('btnRegistro');
-const btnMenu = document.getElementById('btnMenu');
 
-btnRegistro.addEventListener('click', () => {
-  menu.classList.add('hidden');
-  telaRegistro.classList.remove('hidden');
-});
-
-btnMenu.addEventListener('click', () => {
-  telaRegistro.classList.add('hidden');
-  menu.classList.remove('hidden');
-});
+if (btnRegistro) {
+  btnRegistro.addEventListener('click', () => {
+    window.location.href = 'registro.html';
+  });
+}
